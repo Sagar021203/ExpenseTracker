ede5d69ebb5ea602d3c6b494899db2c48bb537f2
Sagarr <150433339+Sagar021203@users.noreply.github.com>
2025-03-09 13:44:09 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server.allowedHosts :gy9fvv-5173.csb.app,
+  server: {
+    allowedHosts: ['gy9fvv-5173.csb.app'],
+  },
 })
